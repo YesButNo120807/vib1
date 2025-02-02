e59720a361574413a4f48e24c33334ce6b8777b2
Maksim <maksaleks120807@gmail.com>
2025-02-02 22:38:40 +0300

    task002

---
+++ task002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.Write($"Введите квадрат числа {number} :");
+int square2 = Convert.ToInt32(Console.ReadLine());
+if (square == square2)
+{
+    Console.Write("Правильно");
+}
+else
+{
+    Console.Write("Неправильно");
+}
